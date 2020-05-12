69d5c071d165a66b25b4c0d4bc420087bf864973
Cellier Pierrick <pcelliervaldesaone@gmail.com>
2020-05-12 17:57:36 +0200

    :construction: import d'un fichier 3D

--- App/Controleur/objet3dController.php
+++ App/Controleur/objet3dController.php
@@ -13,7 +13,7 @@ if($path == "/objet3d_creation"){
   include_once VUE . '/objet3d/objet3d_creation.php';
 }
 if($path == "/objet3d_import"){
-    $file = $_FILES["objet"]["tmp_name"];
-    echo($file);
-   // move_uploaded_file( $file, "../uploads/" . $file);
+
+    $file = $_FILES["objet"]["name"];
+    move_uploaded_file(  $_FILES["objet"]["tmp_name"], APP."/../uploads/" . $file);
 }
