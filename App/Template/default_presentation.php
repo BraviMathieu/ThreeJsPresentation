11a333daf96acdd40ce46026e4e58b84ae15dfc2
Cellier Pierrick <pcelliervaldesaone@gmail.com>
2020-05-20 17:02:57 +0200

    :zap: alternative src pour serveur

--- App/Template/default_presentation.php
+++ App/Template/default_presentation.php
@@ -76,7 +76,7 @@ use App\Session;
         <script>
           const user_id_ajax = "<?=Session::read('User.id')?>";
           const night_mode = "<?=$night_mode?>";
-          const obj3dUrl = "<?=$_SERVER['HTTP_REFERER']."/ThreeJS_Presentation/uploads/"?>";
+          const obj3dUrl = "<?=$_SERVER['HTTP_HOST']."/ThreeJS_Presentation/"?>";
         </script>
       </main>
     </div>
